988654ef42bb2cca21062113db10a1248525f23c
José Orozco <74685832+Slayer1407@users.noreply.github.com>
2022-05-17 16:05:27 -0500

    Add files via upload

---
+++ src/main/java/com/example/http/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.http;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends Activity {
+    private EditText entrada;
+    private TextView salida;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        entrada = (EditText) findViewById(R.id.EditText01);
+        salida = (TextView) findViewById(R.id.TextView01);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.
+                Builder().permitNetwork().build());
+    }
+    public void buscar(View view){
+        try {
+            String palabras = entrada.getText().toString();
+            String resultado = resultadosGoogle(palabras);
+            salida.append(palabras + "--" + resultado + "\n");
+        } catch (Exception e) {
+            salida.append("Error al conectar\n");
+            Log.e("HTTP", e.getMessage(), e);
+        }
+    }
+
+    String resultadosGoogle(String palabras) throws Exception {
+        String pagina = "", devuelve = "";
+        URL url = new URL("http://www.google.es/search?hl=es&q=\""
+                + URLEncoder.encode(palabras, "UTF-8") + "\"");
+        HttpURLConnection conexion = (HttpURLConnection)
+                url.openConnection();
+        conexion.setRequestProperty("User-Agent",
+                "Mozilla/5.0 (Windows NT 6.1)");
+        if (conexion.getResponseCode()==HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new
+                    InputStreamReader(conexion.getInputStream()));
+            String linea = reader.readLine();
+            while (linea != null) {
+                pagina += linea;
+                linea = reader.readLine();
+            }
+            reader.close();
+            devuelve = buscaAproximadamente(pagina);
+        } else {
+            devuelve = "ERROR: " + conexion.getResponseMessage();
+        }
+        conexion.disconnect();
+        return devuelve;
+    }
+    String buscaAproximadamente(String pagina){
+        int ini = pagina.indexOf("Aproximadamente");
+        if (ini != -1) {
+            int fin = pagina.indexOf(" ", ini + 16);
+            return pagina.substring(ini + 16, fin);
+        } else {
+            return "no encontrado";
+        }
+    }
+}
